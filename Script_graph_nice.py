3af019bf784c661571f3c126b8e2bc73dadfee07
Alexis <xyrodileas@gmail.com>
2012-09-21 11:05:47 +0200

    Ajout d'un argument pour choisir le nom du fichier de sortie

--- Script_graph_nice.py
+++ Script_graph_nice.py
@@ -2,9 +2,11 @@ from pygraphviz import *
 
 G=AGraph(strict=True, directed=True)
 #Ajout des noeuds
-G.add_nodes_from([1,2,3,4,5,6,12,15])
+nodes = [1,2,3,4,5,6,12,15]
+G.add_nodes_from(nodes)
 #AJout des arcs
-G.add_edges_from([(1,2),(1,3),(1,4),(1,5),(1,6),(1,12),(1,15),(2,6),(2,12),(3,6),(3,15),(5,15),(6,12)])
+edges = [(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,12),(1,15),(2,6),(2,12),(3,6),(3,15),(5,15),(6,12)]
+G.add_edges_from(edges)
 #Intitulé du graph'
 G.graph_attr['label']='m est un diviseur de n'
 
@@ -13,4 +15,5 @@ G.edge_attr['color']='red'
 
 G.layout(prog='dot')
 #Nom du fichier de sortie
-G.draw('file.png')
+fich = sys.argv[1]+".png"
+G.draw(fich)
